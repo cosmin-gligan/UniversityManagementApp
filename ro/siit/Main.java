0e4dfe5b2ab5cf0b36232a7a74c48c4e90e10c2b
Cosmin <cosmin.gligan@gmail.com>
2021-12-14 21:59:02 +0200

    Definitia temei

---
+++ ro/siit/Main.java
@@ -0,0 +1,27 @@
+package ro.siit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+//        Create the class structure for remaining entities in university diagram, except institution section.
+//        Write the structure in java code with relations between classes (inheritance/composition).
+        /*
+            - university
+                - list of faculties
+            - faculty:
+                - list of courses
+                - list of rooms
+                - list of persons
+            - Rooms:
+                - library
+                - laboratory
+                - amphitheatre
+             - Persons:
+                - Student
+                - Employee
+             Employee:
+         */
+    }
+}
